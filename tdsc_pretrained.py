a6a3aed4c68301743ee83fcfdb4aa3ee8fed9baa
Divam Gupta <divamgupta@gmail.com>
2019-03-03 19:15:02 +0530

    adding lidong pretrained

--- tdsc_pretrained.py
+++ tdsc_pretrained.py
@@ -3,24 +3,24 @@ import json
 import numpy as np
 import keras
 
-# download 16en_UK2_no_ent.h5 and 16en_lidong_mttdsc_deploy_vocab.json from :
-# https://drive.google.com/file/d/1XlFd1Nfl_83VEk5aIvXX0h54AG5hTpAU/view?usp=sharing
-# https://drive.google.com/open?id=1iEfZj3NEEC6Ma6s9voWSvGEdqoAl8mPm
 
 # you need keras 2.1.4 , tensorflow 1.4.0 and python2 to run this
 
 
+model_url = 'https://github.com/divamgupta/mttdsc/releases/download/weights_lidong/16en_lidong_mttdsc_deploy.h5'
+vocab_url = 'https://github.com/divamgupta/mttdsc/releases/download/weights_lidong/16en_lidong_mttdsc_deploy_vocab.json'
 
-model_url = 'https://github.com/divamgupta/mttdsc/releases/download/weights_uk2/16en_UK2_no_ent.1.h5'
-vocab_url = 'https://github.com/divamgupta/mttdsc/releases/download/weights_uk2/16en_lidong_mttdsc_deploy_vocab.1.json'
 
 
-m = load_model( keras.utils.get_file( '16en_UK2_no_ent.h5' , model_url  ) )
-glovevocab = json.loads( open( keras.utils.get_file( '16en_lidong_mttdsc_deploy_vocab.json' , vocab_url  )  ).read() )
+m = load_model( keras.utils.get_file( '16en_lidong_mttdsc_deploy.h5' , model_url  ) )
+glovevocab = json.loads( open( keras.utils.get_file( '16en_lidong_mttdsc_deploy_vocab1.json' , vocab_url  )  ).read() )
 
 
 
 def get_sentiment( sent , target ):
+
+    sent = sent.lower()
+    target = target.lower()
 
     assert (target in sent) , "Target not found in the input sentence "
     sent_l = sent.split(target)[0].strip()
